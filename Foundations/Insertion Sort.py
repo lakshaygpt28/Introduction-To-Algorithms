c4423ad13a028c31e9fa17e3278d024f1b6281cc
Lakshay Gupta <lakshaygpt28@gmail.com>
2017-12-30 15:30:38 +0530

    exercise 2.1-1

---
+++ Foundations/Insertion Sort.py
@@ -0,0 +1,19 @@
+def main():
+	A = [31,41,59,26,41,58]
+	sort_A = Insertion_Sort(list(A))
+	print (A)
+	print (sort_A)
+
+def Insertion_Sort(A1):
+	l = len(A1)
+	for j in range(1,l):
+		key = A1[j]
+		i = j-1
+		while i >= 0 and A1[i] > key:
+			A1[i+1] = A1[i]
+			i -= 1
+		A1[i+1] = key
+	return A1
+
+if __name__ == "__main__":
+	main()
